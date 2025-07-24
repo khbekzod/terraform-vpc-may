8646293648250b85aaffdfc914e13bf244c2ea01
khbekzod <khabibullaevbekzodk@gmail.com>
2025-07-24 00:02:41 +0000

    vpc_name added

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
-provider aws {
-    region = "us-east-1"
-}
-
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "my-vpc"
+    Name = var.vpc_name
   }
 }
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet1_cidr
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -23,7 +19,7 @@ resource "aws_subnet" "main" {
 
 resource "aws_subnet" "main2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet2_cidr
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 
@@ -34,7 +30,7 @@ resource "aws_subnet" "main2" {
 
 resource "aws_subnet" "main3" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet3_cidr
+  cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1c"
   map_public_ip_on_launch = true
 
@@ -78,4 +74,3 @@ resource "aws_route_table_association" "c" {
   subnet_id      = aws_subnet.main3.id
   route_table_id = aws_route_table.example.id
 }
-
